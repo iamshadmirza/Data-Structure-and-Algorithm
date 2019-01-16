dbfdaeb5d892898aff817cc86db4b68f9eabb605
iamshadmirza <md.shad1@yahoo.com>
2019-01-16 18:02:44 +0530

    capitalize, steps done

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,20 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+// function capitalize(str) {
+//     return str.split(' ').reduce((accumulator, element) => {
+//         return accumulator === '' ?
+//             element[0].toUpperCase() + element.slice(1) :
+//             accumulator + " " + element[0].toUpperCase() + element.slice(1);
+//     }, '')
+// }
+
+function capitalize(str) {
+    const words = [];
+    for (let word of str.split(' ')) {
+        words.push(word[0].toUpperCase() + word.slice(1));
+    }
+    return words.join(' ');
+}
 
 module.exports = capitalize;
--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,28 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// function steps(n) {
+//     for (let row = 0; i < n; row++) {
+//         let string = '';
+//         for (let column = 0; column < n; column++) {
+//             column <= row ? string += '#' : string += ' '
+//         }
+//         console.log(string);
+//     }
+// }
+
+function steps(n, row = 0, stair = '') {
+    if (n === row) { return; }
+    if (n === stair.length) {
+        console.log(stair);
+        return steps(n, row + 1);
+    }
+    if (stair.length <= row) {
+        stair += '#';
+    } else {
+        stair += ' ';
+    }
+    steps(n, row, stair);
+}
 
 module.exports = steps;
