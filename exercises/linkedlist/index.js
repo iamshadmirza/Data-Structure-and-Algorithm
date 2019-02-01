282eb629a0b1074dabdb4f53f766a06505d37a40
iamshadmirza <md.shad1@yahoo.com>
2019-02-01 17:40:43 +0530

    insertFirst node in linked list

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -13,6 +13,9 @@ class LinkedList {
     constructor() {
         this.head = null;
     }
+    insertFirst(data) {
+        this.head = new Node(data, this.head);
+    }
 }
 
 module.exports = { Node, LinkedList };
