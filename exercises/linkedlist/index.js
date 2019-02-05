7809ba1799b5666f7448486bfe8f0c02fecdc58d
iamshadmirza <md.shad1@yahoo.com>
2019-02-05 23:31:54 +0530

    removeAt(index)

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -83,6 +83,17 @@ class LinkedList {
         }
         return null;
     }
+    removeAt(index) {
+        if (!this.head) { return }
+        if (index === 0) {
+            this.head = this.head.next;
+            return;
+        }
+        const previous = this.getAt(index - 1);
+        if (!previous || !previous.next) { return; }
+        previous.next = previous.next.next;
+
+    }
 }
 
 module.exports = { Node, LinkedList };
