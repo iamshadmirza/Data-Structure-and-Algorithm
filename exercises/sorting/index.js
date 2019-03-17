83e9464eda388e60b064cc3eb44d06527160a4b0
iamshadmirza <md.shad1@yahoo.com>
2019-03-17 12:50:21 +0530

    bubble sort and selection sort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -2,11 +2,31 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
-
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < (arr.length - i - 1); j++) {
+            if (arr[j] > arr[j + 1]) {
+                const lesser = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = lesser;
+            }
+        }
+    }
+    return arr;
 }
 
 function selectionSort(arr) {
-
+    for (let i = 0; i < arr.length; i++) {
+        let indexOfMin = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[indexOfMin]) {
+                indexOfMin = j;
+            }
+        }
+        const lesser = arr[indexOfMin];
+        arr[indexOfMin] = arr[i];
+        arr[i] = lesser;
+    }
+    return arr;
 }
 
 function mergeSort(arr) {
