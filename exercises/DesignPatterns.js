23f80f250220850d29fa896d2962c4f0e792aa03
iamshadmirza <md.shad1@yahoo.com>
2019-04-12 07:01:54 +0530

    Singleton and Factory

---
+++ exercises/DesignPatterns.js
@@ -0,0 +1,90 @@
+//Singleton Pattern
+var Singleton = (function () {
+    var instance;
+    function createInstance() {
+        var object = new Object('This is an instance');
+        return object;
+    }
+    return {
+        getInstance: function () {
+            if (!instance) {
+                instance = createInstance();
+            }
+            return instance;
+        }
+    };
+})();
+
+function run() {
+    var instance1 = Singleton.getInstance();
+    var instance2 = Singleton.getInstance();
+    console.log('Same instance: ', instance1 === instance2);
+}
+
+//Factory Pattern 
+function Factory() {
+    this.createEmployee = function (type) {
+        var employee;
+
+        if (type === "fulltime") {
+            employee = new FullTime();
+        } else if (type === "parttime") {
+            employee = new PartTime();
+        } else if (type === "temporary") {
+            employee = new Temporary();
+        } else if (type === "contractor") {
+            employee = new Contractor();
+        }
+
+        employee.type = type;
+
+        employee.say = function () {
+            log.add(this.type + ": rate " + this.hourly + "/hour");
+        }
+
+        return employee;
+    }
+}
+
+var FullTime = function () {
+    this.hourly = "$12";
+};
+
+var PartTime = function () {
+    this.hourly = "$11";
+};
+
+var Temporary = function () {
+    this.hourly = "$10";
+};
+
+var Contractor = function () {
+    this.hourly = "$15";
+};
+
+// log helper
+var log = (function () {
+    var log = "";
+
+    return {
+        add: function (msg) { log += msg + "\n"; },
+        show: function () { alert(log); log = ""; }
+    }
+})();
+
+function run() {
+    var employees = [];
+    var factory = new Factory();
+
+    employees.push(factory.createEmployee("fulltime"));
+    employees.push(factory.createEmployee("parttime"));
+    employees.push(factory.createEmployee("temporary"));
+    employees.push(factory.createEmployee("contractor"));
+
+    for (var i = 0, len = employees.length; i < len; i++) {
+        employees[i].say();
+    }
+
+    log.show();
+}
+
